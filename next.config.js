33ef866f0f9221651dc29722ba34899560bc0e3b
Justin O'Boyle <justin@justinoboyle.com>
2022-01-11 15:35:31 -0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        // temporary until I make something here
+        destination: "https://www.linkedin.com/in/justinoboyle",
+        permanent: false,
+      },
+    ];
+  },
 }
